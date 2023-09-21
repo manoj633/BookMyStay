b0ae57bd53cb58ee8b0f9ff52a53b3f3df7de0e8
Manoj <manoj09633@gmail.com>
2023-09-21 23:29:13 +0530

    MonngoDb backend integration

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected successfully!");
+  } catch (error) {
+    throw error;
+  }
+};
+
 app.listen(8800, () => {
+  connect();
   console.log("connected to backend");
 });
