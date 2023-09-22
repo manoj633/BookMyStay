941122365116c92a23be17c229438ccd409143b7
Manoj <manoj09633@gmail.com>
2023-09-22 13:30:03 +0530

    Created endpoints for delete, get and get all hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -14,19 +14,52 @@ router.post("/", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
 // UPDATE
 router.put("/:id", async (req, res) => {
   try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {
-      $set: req.body,
-    });
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
     res.status(200).json(updatedHotel);
   } catch (error) {
     res.status(500).json(error);
   }
 });
+
 // DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).send("Hotel deleted");
+    console.log("Hotel deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // GET
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // GET ALL
+router.get("/", async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
