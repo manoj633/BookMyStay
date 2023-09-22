7df765e1dcf94222f08472f7ec6be1cdb0cf8ea4
Manoj <manoj09633@gmail.com>
2023-09-22 19:45:05 +0530

    Hashed the entered password using bcryptjs

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,11 +1,14 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword,
     });
 
     await newUser.save();
