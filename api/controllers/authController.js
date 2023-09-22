40e431d16960d039d3a7fb2c8e9361b1dd753afc
Manoj <manoj09633@gmail.com>
2023-09-22 19:58:31 +0530

    Added login method and route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -13,6 +14,29 @@ export const register = async (req, res, next) => {
 
     await newUser.save();
     res.status(201).send("User has been created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return next(createError(404, "User not found"));
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect) {
+      return next(createError(400, "Password incorrect"));
+    }
+
+    const { password, isAdmin, _id, createdAt, updatedAt, __v, ...others } =
+      user._doc;
+    res.status(201).json(others);
   } catch (error) {
     next(error);
   }
