2786abb690bbc7e6aed5128ace43042026fb44d9
Manoj <manoj09633@gmail.com>
2023-09-24 13:53:14 +0530

    Added footer to Home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/propertyList/PropertyList";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import MailList from "../../components/mailList/MailList";
+import Footer from "../../components/footer/Footer";
 
 function Home() {
   return (
@@ -19,6 +20,7 @@ function Home() {
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
         <MailList />
+        <Footer />
       </div>
     </div>
   );
