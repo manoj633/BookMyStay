08d5c4f706346083ea550171568f37ae18c88045
Manoj <manoj09633@gmail.com>
2023-09-24 13:44:46 +0530

    Added featured component to home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import NavBar from "../../components/navbar/NavBar";
 import Header from "../../components/header/Header";
+import "./home.css";
+import Featured from "../../components/featured/Featured";
 
 function Home() {
   return (
     <div>
       <NavBar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+      </div>
     </div>
   );
 }
