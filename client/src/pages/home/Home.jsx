efc135785cb974d25468d3f4d7f364b259d60ebf
Manoj <manoj09633@gmail.com>
2023-09-24 13:48:41 +0530

    Added featured properties list component to Home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Header from "../../components/header/Header";
 import "./home.css";
 import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 
 function Home() {
   return (
@@ -14,6 +15,8 @@ function Home() {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   );
