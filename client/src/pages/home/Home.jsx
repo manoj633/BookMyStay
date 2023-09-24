4f251ed24b390ab3692a808d40fc602a3ca111fd
Manoj <manoj09633@gmail.com>
2023-09-24 13:42:11 +0530

    Created Header component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../../components/navbar/NavBar";
+import Header from "../../components/header/Header";
 
 function Home() {
   return (
     <div>
       <NavBar />
+      <Header />
     </div>
   );
 }
