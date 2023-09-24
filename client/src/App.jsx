71bf7aa955d65286af293dc3b48eff7331b171b0
Manoj <manoj09633@gmail.com>
2023-09-24 13:33:17 +0530

    Added navbar component in home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <>
-      <p>Hello world</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../../components/navbar/NavBar";
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default Home;
