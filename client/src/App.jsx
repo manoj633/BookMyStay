b0a1860d9c8cc2ae430d46fcf05f9d420b6662f8
Manoj <manoj09633@gmail.com>
2023-09-24 20:16:35 +0530

    Added list hotels based on seach component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<Home />} />
+        <Route path={"/hotels"} element={<List />} />
       </Routes>
     </BrowserRouter>
   );
