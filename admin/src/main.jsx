aa9caf52135be5295b356e8bbc8a63370f46426e
Manoj <manoj09633@gmail.com>
2023-09-26 20:48:23 +0530

    Provided dark mode and authentication context

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { DarkModeContextProvider } from "./context/darkModeContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AuthContextProvider>
+      <DarkModeContextProvider>
+        <DarkModeContextProvider></DarkModeContextProvider>
+      </DarkModeContextProvider>
+    </AuthContextProvider>
+
     <App />
   </React.StrictMode>
 );
