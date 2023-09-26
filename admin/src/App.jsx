8b596fdb9cf48e96f35a2748d39cd8ab9f503ecb
Manoj <manoj09633@gmail.com>
2023-09-26 21:09:12 +0530

    Created Home page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/">
+            <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
           </Route>
         </Routes>
